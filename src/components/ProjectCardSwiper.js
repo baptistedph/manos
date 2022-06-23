43c5596330f7bc00d2c69ab8bde303fbfa1abaaf
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-06-23 11:24:26 +0200

    fix: deleting a few things and pushing before merging main into my branch

--- src/components/ProjectCardSwiper.js
+++ src/components/ProjectCardSwiper.js
@@ -6,27 +6,24 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination, Navigation } from "swiper";
+import { Navigation } from "swiper";
 
 const ProjectCardSwiper = () => {
 	return (
 		<div>
 			<>
 				<Swiper
-					pagination={{
-						type: "fraction",
-					}}
 					navigation={true}
-					modules={[Pagination, Navigation]}
+					modules={[Navigation]}
 					className="mySwiper"
 				>
 					{/* after testing with pictures of different sizes, it appears the width is fix. */}
-					<SwiperSlide className="project_swiper_slide"><img src="./assets/test_slider/img1.jpg" alt="image fairytail 1" className="project_swiper_img" /></SwiperSlide>
-					<SwiperSlide className="project_swiper_slide"><img src="./assets/test_slider/img2.jpg" alt="image fairytail 2" className="project_swiper_img" /></SwiperSlide>
-					<SwiperSlide className="project_swiper_slide"><img src="./assets/test_slider/img3.jpg" alt="peinture roi lion 1" className="project_swiper_img" /></SwiperSlide>
-					<SwiperSlide className="project_swiper_slide"><img src="./assets/test_slider/img4.jpg" alt="peinture roi lion 2" className="project_swiper_img" /></SwiperSlide>
-					<SwiperSlide className="project_swiper_slide"><img src="./assets/test_slider/img5.jpg" alt="peinture roi lion 3" className="project_swiper_img" /></SwiperSlide>
-					<SwiperSlide className="project_swiper_slide"><img src="./assets/test_slider/img6.jpg" alt="peinture roi lion 4" className="project_swiper_img" /></SwiperSlide>
+					<SwiperSlide className="project_swiper_slide"><img src="./assets/project_card_pics/img1.jpg" alt="image fairytail 1" className="project_swiper_img" /></SwiperSlide>
+					<SwiperSlide className="project_swiper_slide"><img src="./assets/project_card_pics/img2.jpg" alt="image fairytail 2" className="project_swiper_img" /></SwiperSlide>
+					<SwiperSlide className="project_swiper_slide"><img src="./assets/project_card_pics/img3.jpg" alt="peinture roi lion 1" className="project_swiper_img" /></SwiperSlide>
+					<SwiperSlide className="project_swiper_slide"><img src="./assets/project_card_pics/img4.jpg" alt="peinture roi lion 2" className="project_swiper_img" /></SwiperSlide>
+					<SwiperSlide className="project_swiper_slide"><img src="./assets/project_card_pics/img5.jpg" alt="peinture roi lion 3" className="project_swiper_img" /></SwiperSlide>
+					<SwiperSlide className="project_swiper_slide"><img src="./assets/project_card_pics/img6.jpg" alt="peinture roi lion 4" className="project_swiper_img" /></SwiperSlide>
 				</Swiper>
 			</>
 		</div>
