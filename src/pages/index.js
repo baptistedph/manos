a4168ff79dd2301401754467991899f3559747e8
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-06-23 16:58:05 +0200

    feat: tab bar for the profile settings

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,42 +2,18 @@ import UsersTable from '../components/UsersTable'
 import { Flex, Heading, Container } from '@chakra-ui/react'
 import { fetchApi } from '../lib/api'
 import "swiper/css/bundle";
+import TabBarProfileSettings from '../components/TabBarProfileSettings';
 import ProjectCard from '../components/ProjectCard';
 
-// export const getServerSideProps = async ({ req }) => {
-
-// 	const users = await fetchApi('/users')
-
-// 	return {
-// 		props: {
-// 			users,
-// 		},
-// 	}
-// }
 
 const Home = ({ users }) => {
 
 	return (
 		<div>
-			<div>
-				<ProjectCard />
-				{/* // <Flex
-			// 	alignItems="center"
-			// 	flexDirection="column"
-			// 	justifyContent="center"
-			// 	h="100vh">
-			// 	<Container maxWidth={500}>
-			// 		<Heading>Users</Heading>
-			// 		<UsersTable users={users} />
-			// 	</Container>
-			// </Flex> */}
-				{/* <Flex justifyContent="center" alignItems="center" h="100vh">
-					<Container maxW="4xl">
-						<Heading>Users</Heading>
-						<UsersTable users={users} />
-					</Container>
-				</Flex> */}
-			</div>
+			<ProjectCard />
+			<br />
+			<br />
+			<TabBarProfileSettings />
 		</div>
 	)
 }
