3a9f92b094bd7646ab1d373e8b8d1e342212c3a5
HyppolitePrn <pernothyppolite@gmail.com>
2022-06-23 17:40:05 +0200

    feat: contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Image } from "@chakra-ui/react";
+import { Grid, GridItem, Heading, Flex } from "@chakra-ui/react";
 
 const Contact = () => {
   const infos = [
@@ -11,22 +11,29 @@ const Contact = () => {
   ];
   return (
     <div className="container">
-      <Grid templateColumns="repeat(2, 1fr)" gap={6} m={4}>
-        <GridItem colSpan={2}>Contact</GridItem>
+      <Grid templateColumns="repeat(2, 1fr)" mt={5} ml={4} mb={5}>
+        <GridItem colSpan={2} mb={3}>
+          <Heading as="h1" size="sm">
+            Contact
+          </Heading>
+        </GridItem>
         {infos.map((info) => (
-          <GridItem key={info} as="samp">
-            {info}
+          <GridItem key={info} as="samp" mb={2.5}>
+            <Heading size="xs">{info}</Heading>
           </GridItem>
         ))}
-        <GridItem colSpan={2} as="samp">
-          Localisation
-        </GridItem>
       </Grid>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97138.01641568248!2d2.232014774065856!3d48.87020059548142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!5e0!3m2!1sfr!2sfr!4v1655900798693!5m2!1sfr!2sfr"
-        width="100%"
-        height="450"
-      ></iframe>
+      <Heading size="sm" ml={4} mb={3}>
+        Localisation
+      </Heading>
+      <Flex justify="center">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97138.01641568248!2d2.232014774065856!3d48.87020059548142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!5e0!3m2!1sfr!2sfr!4v1655900798693!5m2!1sfr!2sfr"
+          width="90%"
+          height="450"
+          style={{ borderRadius: "6px" }}
+        ></iframe>
+      </Flex>
     </div>
   );
 };
