decdb01215377afe738e32bc87e3e97320d45d2a
Cammyxalot <renoir.fab@outlook.com>
2022-06-24 13:49:54 +0200

    feat:responsive

--- src/pages/profilHeader.js
+++ src/pages/profilHeader.js
@@ -22,60 +22,66 @@ const ProfilBanner = () => {
     profilName: "Tic-Tac Bois",
   };
   return (
-    <Box
-      w="23.438rem"
-      borderRadius="6px"
-      maxW="375"
-      color="black"
-      h="min-content"
-    >
+    <Box mx="auto" maxW="5xl">
       <Image
-        w="375px"
-        h="100px"
+        w="100%"
+        height={{ base: "100px", sm: "175px", md: "250px" }}
         objectFit="cover"
         objectPosition="100% 0"
         src={prop.profilImage}
         alt={prop.altprop}
       />
-      <Image
-        borderRadius="full"
-        src="https://bit.ly/dan-abramov"
-        alt="Dan Abramov"
-        position="absolute"
-        bottom="210px"
-        ml="16px"
-        w="90px"
-        h="90px"
-        objectFit="cover"
-        objectPosition="100% 0"
-      />
-      <Box position="relative" left="290px" top="15px" w="30px" display="flex">
-        <Button
-          variant="outline"
-          colorScheme="teal"
-          mr="10px"
-          borderRadius="50px"
-          size="sm"
-        >
-          <FontAwesomeIcon icon={faEnvelope} color="teal">
-            {" "}
-          </FontAwesomeIcon>
-        </Button>
-        <Button
-          variant="outline"
-          colorScheme="teal"
-          mr="10px"
-          borderRadius="50px"
-          size="sm"
-        >
-          <FontAwesomeIcon icon={faShareNodes} color="teal"></FontAwesomeIcon>
-        </Button>
-      </Box>
-      <Box pt="30px">
-        <Heading as="h4" size="md" ml="16px">
+      <Flex
+        justify="space-between"
+        h={{ base: "40px", sm: "50px", md: "60px" }}
+      >
+        <Image
+          position="relative"
+          bottom={{ base: "45px", sm: "60px", md: "67.5" }}
+          borderRadius="full"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+          ml="16px"
+          width={{ base: "90px", sm: "120px", md: "135px" }}
+          height={{ base: "90px", sm: "120px", md: "135px" }}
+          objectFit="cover"
+          objectPosition="100% 0"
+        />
+        <Box display="flex" pt="12px">
+          <Button
+            w={{ base: "28px", sm: "40px", md: "50px" }}
+            h={{ base: "28px", sm: "40px", md: "50px" }}
+            size={{ base: "sm", sm: "md", md: "xl" }}
+            variant="outline"
+            colorScheme="teal"
+            mr="10px"
+            borderRadius="50px"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="teal"></FontAwesomeIcon>
+          </Button>
+          <Button
+            w={{ base: "28px", sm: "40px", md: "50px" }}
+            h={{ base: "28px", sm: "40px", md: "50px" }}
+            size={{ base: "sm", sm: "sm", md: "xl" }}
+            variant="outline"
+            colorScheme="teal"
+            mr="10px"
+            borderRadius="50px"
+          >
+            <FontAwesomeIcon icon={faShareNodes} color="teal"></FontAwesomeIcon>
+          </Button>
+        </Box>
+      </Flex>
+      <Box>
+        <Heading as="h4" size="md" ml="16px" mt="20px">
           Tic-Tac
         </Heading>
-        <Text fontSize="12px" ml="16px" mt="10px" mr="16px">
+        <Text
+          fontSize={{ base: "12px", md: "16px", lg: "18px" }}
+          ml="16px"
+          mt="10px"
+          mr="16px"
+        >
           Tic-Tac, c’est mon rêve d’enfant. Fan de bricolage avant même de
           savoir marcher, formé à l’école Boulle, je travaille le bois depuis 7
           ans déjà.
@@ -94,12 +100,36 @@ const ProfilBanner = () => {
         </Text>
       </Box>
       <Flex align="center" justify="center" mt={5}>
-        <Tabs variant="line" colorScheme="teal">
+        <Tabs
+          variant="line"
+          colorScheme="teal"
+          w={{ base: "sm", sm: "md", md: "3xl", lg: "4xl" }}
+        >
           <TabList color="teal">
-            <Tab>Projet</Tab>
-            <Tab>Planning</Tab>
-            <Tab>Contact</Tab>
-            <Tab>Réseau</Tab>
+            <Tab
+              w={{ base: "sm", sm: "md", md: "xl" }}
+              fontSize={{ base: "14px", md: "16px", lg: "18px" }}
+            >
+              Projet
+            </Tab>
+            <Tab
+              w={{ base: "sm", sm: "md", md: "xl" }}
+              fontSize={{ base: "14px", md: "16px", lg: "18px" }}
+            >
+              Planning
+            </Tab>
+            <Tab
+              w={{ base: "sm", sm: "md", md: "xl" }}
+              fontSize={{ base: "14px", md: "16px", lg: "18px" }}
+            >
+              Contact
+            </Tab>
+            <Tab
+              w={{ base: "sm", sm: "md", md: "xl" }}
+              fontSize={{ base: "14px", md: "16px", lg: "18px" }}
+            >
+              Réseau
+            </Tab>
           </TabList>
 
           <TabPanels>
