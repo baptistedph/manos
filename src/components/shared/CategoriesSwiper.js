e32fbed902b4cd628ddb39618e213ac96eb3dc8b
Thierry Maignan <maignan.thierry@outlook.fr>
2022-06-25 11:28:09 +0200

    style: responsive slider home

--- src/components/shared/CategoriesSwiper.js
+++ src/components/shared/CategoriesSwiper.js
@@ -1,19 +1,22 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import Category from './Category'
-import { Box, Flex, Heading } from '@chakra-ui/react'
+import { Swiper, SwiperSlide } from "swiper/react"
+import Category from "./Category"
+import { Box, Flex, Heading } from "@chakra-ui/react"
+import useMediaQuery from "@mui/material/useMediaQuery"
 
-import 'swiper/css'
+import "swiper/css"
 
 const CategoriesSwiper = ({ title, categories = [] }) => {
+  const matches = useMediaQuery("(min-width:480px)")
   return (
     <Flex flexDirection="column" gap={3}>
       <Heading size="sm">{title}</Heading>
       <Box maxW="100%">
         <Swiper
           spaceBetween={12}
-          slidesPerView={4}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={swiper => console.log(swiper)}>
+          slidesPerView={matches ? 3 : 2}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
           {categories.map((category, i) => {
             return (
               <SwiperSlide key={i}>
