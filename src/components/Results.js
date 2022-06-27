bd6667b446a47fc346b50b9307ffc6704c8d3d73
Baptiste Dauphouy <bdauphouy@gmail.com>
2022-06-27 23:20:42 +0200

    feat: network

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,15 +1,20 @@
-import { Flex, Heading, Box } from "@chakra-ui/react"
-import ProfileCard from "./shared/ProfileCard"
+import { Flex, Heading, Box } from '@chakra-ui/react'
+import ProfileCard from './shared/ProfileCard'
 
 const Results = ({ search, data }) => {
   return (
     <Flex flexDirection="column" gap={5} mt={5}>
       <Heading size="sm">
-        {data.length} {data.length > 1 ? "résultats" : "résultat"} pour {search}
+        {data.length} {data.length > 1 ? 'résultats' : 'résultat'} pour {search}
       </Heading>
-      <Flex justify="space-between" align="baseline" wrap="wrap" gap={5} className="profils">
-        {data.map((user) => (
-          <Box w={{base: "350px", lg:"420px"}}>
+      <Flex
+        justify="space-between"
+        align="baseline"
+        wrap="wrap"
+        gap={5}
+        className="profils">
+        {data.map((user, i) => (
+          <Box key={i} w={{ base: '350px', lg: '420px' }}>
             <ProfileCard user={user} />
           </Box>
         ))}
