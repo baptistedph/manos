4c84590e2769ca6910ef5c1b58a6083a8d770554
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-06-27 11:42:47 +0200

    style: responsive on project page

--- src/components/colab.js
+++ src/components/colab.js
@@ -2,68 +2,69 @@ import { Box, Flex } from '@chakra-ui/react';
 import "swiper/css";
 import "swiper/css/pagination";
 import React from 'react';
-import {Swiper ,SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from "swiper";
 
 const Colab = () => {
-    const src='./assets/pictures/profile.jpg'
+	const src = './assets/pictures/profile.jpg'
 
-    return (
-    <Flex
-    justifyContent='center'
-        >
-        <Box
-            ml="1em"
-            mt="1.5em"
-            maxWidth="23.438em"
-            
-        >
-        <h2 style={{fontSize:1 + "em", marginTop:1.25 + "em", marginBottom:0.5 + 'em' ,fontWeight:'bold'}} >Collaborateur</h2>
-        <Swiper
-                slidesPerView={7}
-                spaceBetween={6}
-                modules={[Pagination]}
-                className="Colabs"
-        >
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
+	return (
+		<Flex
+			justifyContent='center'
+		>
+			<Box
+				ml="1em"
+				mt="1.5em"
+				minWidth="23.438em"
+				maxWidth="650px"
 
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
+			>
+				<h2 style={{ fontSize: 1 + "em", marginTop: 1.25 + "em", marginBottom: 0.5 + 'em', fontWeight: 'bold' }} >Collaborateur</h2>
+				<Swiper
+					slidesPerView={9}
+					spaceBetween={7}
+					modules={[Pagination]}
+					className="Colabs"
+				>
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
 
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
 
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
 
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
 
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
 
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
 
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
 
-            <SwiperSlide>
-                <img src={src} className='Collab'/>
-            </SwiperSlide>
-            </Swiper>
-        </Box>
-    </Flex>
-    )           
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
+
+					<SwiperSlide>
+						<img src={src} className='Collab' />
+					</SwiperSlide>
+				</Swiper>
+			</Box>
+		</Flex>
+	)
 }
- 
+
 export default Colab;
--- src/components/project.js
+++ src/components/project.js
@@ -11,58 +11,59 @@ import { Pagination } from "swiper";
 import { Box, Flex } from "@chakra-ui/react";
 
 const Project = () => {
-  const img = 'https://cache.marieclaire.fr/data/photo/w1200_h630_c17/5o/idees-deco-terrasse.jpg'
-  const name= 'Slide'
-  
-    return ( 
-    <Flex
-    justifyContent='center'
-    >
-      <Box
-        ml="1em"
-        mt="1.5em"
-        maxW="23.438em"
-        p={0}
-      >
-      <h2 style={{fontSize:1 + "em", marginTop:1.25 + "em", marginBottom:0.5 + 'em',fontWeight:'bold'}} >Autre projet</h2>
-        <Swiper
-        slidesPerView={3}
-        spaceBetween={10}
-        modules={[Pagination]}
-        className="projectSwiper"
-      >
-        <SwiperSlide className="projectSlide" style={{marginLeft: 1 + "em" }}>
-            <img src={img} alt='img'/>
-            {name}
-        </SwiperSlide>
+	const img = 'https://cache.marieclaire.fr/data/photo/w1200_h630_c17/5o/idees-deco-terrasse.jpg'
+	const name = 'Slide'
 
-        <SwiperSlide className="projectSlide">
-          <img src={img} alt='img'/>
-          {name}
-        </SwiperSlide>
+	return (
+		<Flex
+			justifyContent='center'
+		>
+			<Box
+				ml="1em"
+				mt="1.5em"
+				minW="23.438em"
+				maxWidth="650px"
+				p={0}
+			>
+				<h2 style={{ fontSize: 1 + "em", marginTop: 1.25 + "em", marginBottom: 0.5 + 'em', fontWeight: 'bold' }} >Autre projet</h2>
+				<Swiper
+					slidesPerView={3}
+					spaceBetween={10}
+					modules={[Pagination]}
+					className="projectSwiper"
+				>
+					<SwiperSlide className="projectSlide" style={{ marginLeft: 1 + "em" }}>
+						<img src={img} alt='img' />
+						{name}
+					</SwiperSlide>
 
-        <SwiperSlide className="projectSlide">
-          <img src={img} alt='img'/>
-          {name}
-        </SwiperSlide>
-        <SwiperSlide className="projectSlide">
-          <img src={img} alt='img'/>
-          {name}
-        </SwiperSlide>
+					<SwiperSlide className="projectSlide">
+						<img src={img} alt='img' />
+						{name}
+					</SwiperSlide>
 
-        <SwiperSlide className="projectSlide">
-          <img src={img} alt='img'/>
-          {name}
-        </SwiperSlide>
+					<SwiperSlide className="projectSlide">
+						<img src={img} alt='img' />
+						{name}
+					</SwiperSlide>
+					<SwiperSlide className="projectSlide">
+						<img src={img} alt='img' />
+						{name}
+					</SwiperSlide>
 
-        <SwiperSlide className="projectSlide">
-          <img src={img} alt='img'/>
-          {name}
-        </SwiperSlide>
-      </Swiper>
-      </Box>
-      </Flex>
-     );
+					<SwiperSlide className="projectSlide">
+						<img src={img} alt='img' />
+						{name}
+					</SwiperSlide>
+
+					<SwiperSlide className="projectSlide">
+						<img src={img} alt='img' />
+						{name}
+					</SwiperSlide>
+				</Swiper>
+			</Box>
+		</Flex>
+	);
 }
- 
+
 export default Project;
