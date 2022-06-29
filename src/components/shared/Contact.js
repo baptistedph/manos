f78e13de8334ebb95281d1fd32c66de1326ad2d4
HyppolitePrn <pernothyppolite@gmail.com>
2022-06-29 15:25:26 +0200

    fix: map on contact

--- src/components/shared/Contact.js
+++ src/components/shared/Contact.js
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Heading, Flex, Box } from "@chakra-ui/react"
+import { Grid, GridItem, Heading, Flex, Box } from "@chakra-ui/react";
 
 const Contact = ({ phone, street, email, post_code, city }) => {
   return (
@@ -101,7 +101,7 @@ const Contact = ({ phone, street, email, post_code, city }) => {
         </Heading>
         <Flex justify="center" w={{ base: "90%", maxW: "fill container" }}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97138.01641568248!2d2.232014774065856!3d48.87020059548142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!5e0!3m2!1sfr!2sfr!4v1655900798693!5m2!1sfr!2sfr"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.551974763433!2d2.3964251155546044!3d48.84768397928649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e672709c9e2a97%3A0x9339865fdfa3f1e5!2s12%20Av.%20du%20Tr%C3%B4ne%2C%2075012%20Paris!5e0!3m2!1sfr!2sfr!4v1656509028716!5m2!1sfr!2sfr"
             width="100%"
             height="300"
             style={{ borderRadius: "6px" }}
@@ -109,7 +109,7 @@ const Contact = ({ phone, street, email, post_code, city }) => {
         </Flex>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
